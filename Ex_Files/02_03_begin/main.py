d60db0753ab8df84f4c7e803ae4733b8618865fc
gradio <gradylawlor@gmail.com>
2023-01-18 07:30:02 +0000

    Added notes to remind of list order syntax

--- Ex_Files/02_03_begin/main.py
+++ Ex_Files/02_03_begin/main.py
@@ -6,6 +6,11 @@ PAUL = NAMES[1]
 
 JOHN_PAUL = NAMES[:2]
 GEORGE_RINGO = NAMES[2:]
+"""[a:b:c]
+a = Start
+b = Stop
+c = Step
+(if Step is negative, reverse order by n steps)"""
 REVERSE = NAMES[::-1]
 EVERY_OTHER = NAMES[::2]
 
